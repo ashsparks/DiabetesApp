97b9783dfb9d8f8cd05e65e43c014ee98486a535
Ash Sparks <ash.sparks@yahoo.co.uk>
2014-12-25 15:56:19 +0000

    Responsive CSS Addition
    CSS Added
    Viewport added

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width">
+<title>Diabetic App</title>
+<link rel="stylesheet" type="text/css" href="theme.css" />
+<link rel="stylesheet" media="screen and (min-device-width: 1440px)" href="desktopTheme.css" />
+<link rel="stylesheet" media="screen and (min-width: 800px) and (max-width: 1439px)" href="tabletTheme.css" />
+<link rel="stylesheet" media="screen and (min-width: 100px) and (max-width: 799px)" href="mobileTheme.css" />
+</head>
+<body>
+<header>
+</header>
+<nav>
+<ol><li>Ex1</li>
+<li>Ex2</li>
+<li>Ex3</li></ol>
+</nav>
+<footer>
+<p>Ash Sparks(c)2014</p>
+</footer>
+</body>
+</html>
